c2096400ed6a2b607fdd30d88b36a1cb76afa866
wy7318 <46336956+wy7318@users.noreply.github.com>
2019-02-12 22:03:34 -0700

    Create forprogram.py

---
+++ basic/forprogram.py
@@ -0,0 +1,11 @@
+for state in ["not pinin","no more"]:
+    #print("this is "+state)
+    print("this is {}".format(state))
+
+for i in range(0, 100, 3):
+    print("i is {}".format(i))
+
+for i in range(1,10):
+    for j in range(1,10):
+        print("{} times {} is {}".format(i,j,i*j))
+    print("===================")
