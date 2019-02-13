25f77dc4166380fc576db137de4ef445c18d96fa
wy7318 <46336956+wy7318@users.noreply.github.com>
2019-02-12 22:34:17 -0700

    Update forprogram.py

--- basic/forprogram.py
+++ basic/forprogram.py
@@ -2,6 +2,8 @@ for state in ["not pinin","no more"]:
     #print("this is "+state)
     print("this is {}".format(state))
 
+    
+    
 #0 through 100 with 3 increment
 for i in range(0, 100, 3):
     print("i is {}".format(i))
@@ -11,13 +13,31 @@ for i in range(1,10):
         print("{} times {} is {}".format(i,j,i*j))
     print("===================")
 
+    
+    
 #printing with condition
 quote = """
 Alright, but apart from the Sanitation, the Medicine, Education, Wine,
 Public Order, Irrigation, Roads, the Fresh-Water System,
 and Public Health, what have the Romans ever done for us?
 """
-
 for i in quote:
     if i in 'ZXCVBNMLKJHGFDSAWQRTYUIOP':
         print(i)
+
+
+#difference between continue and break
+shopping_list=["mile", "egg", "water","spam","beer"]
+for item in shopping_list:
+    if item =='spam':
+        continue
+    print("buy "+item)
+
+shopping_list=["mile", "egg", "water","spam","beer"]
+for item in shopping_list:
+    if item =='spam':
+        nastyItem=item
+        break
+if nastyItem:
+    print("I can't havee anything without spam")
+        
