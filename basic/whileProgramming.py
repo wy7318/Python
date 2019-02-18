bebf01f3aff84103ed082f712a1568ea01815fd5
wy7318 <46336956+wy7318@users.noreply.github.com>
2019-02-18 14:20:22 -0700

    Update whileProgramming.py

--- basic/whileProgramming.py
+++ basic/whileProgramming.py
@@ -7,3 +7,27 @@ while choosen_exit not in available_exit:
         break
 else:
     print("glad you got out")
+
+###################################################################################
+    
+#guessing number 
+import random
+
+highest = 10
+answer = random.randint(1, highest)
+
+print("guess a number between 1 and {}".format(highest))
+guess = int(input())
+
+while guess != answer:
+    if guess != answer:
+        if guess < answer:
+            print("please guess higher")
+        else:
+            print("guess lower")
+        guess = int(input())
+    if guess == answer:
+        print("Well done")
+        break
+else:
+    print("got it right first time")
