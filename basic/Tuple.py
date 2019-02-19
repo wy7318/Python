ec11a8f2994fb1721a179d2286ef244e043eb9ef
wy7318 <46336956+wy7318@users.noreply.github.com>
2019-02-18 21:51:51 -0700

    Update Tuple.py

--- basic/Tuple.py
+++ basic/Tuple.py
@@ -1,7 +1,7 @@
 welcome = "Welcome to my nightmare", "Alice Cooper", 1975
 bad = "Bad Company","Bad Company", 1974
 budgie = "Nightflight", "Budgie", 1981
-imelda ="More Mayhem", "Emilda May", 2011
+imelda ="More Mayhem", "Emilda May", 2011, ((1, "Pulling the Rug"), (2, "Psycho"), (3, "Nayhem"))
 metallica = "Ride the Lightning", "Metallica", 1984
 
 print(metallica)
@@ -13,6 +13,21 @@ metallica2 =["Ride the Lightning", "Metallica", 1984]
 print(metallica2)
 metallica2[0] ="Master of Puppets" #In the case of list, we can assign new value right away
 print(metallica2)
+
+title, artist, year, track = imelda #assigning 
+print(title)
+print(artist)
+print(year)
+print(track)
+
+#=====================================================================================
+
+print(title)
+print(artist)
+print(year)
+for song in track:
+    track1, title = song
+    print("\tTrack Number: {}, Title: {}".format(track1, title))
 
 #==============================================================================
 
