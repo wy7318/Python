f37517ef9510a3bc3062eb882f4ba9ac8fed38a9
wy7318 <46336956+wy7318@users.noreply.github.com>
2019-02-18 21:05:14 -0700

    Create RangeProgramming.py

---
+++ basic/RangeProgramming.py
@@ -0,0 +1,12 @@
+my_list = list(range(20)) #creating list using range
+print(my_list)
+
+even = list(range(0, 10, 2)) #list of number incremented by 2, start from 0 to 10
+odd = list(range(1, 10, 2)) #list of number incremented by 2, start from 1 to 10
+
+print(even)
+print(odd)
+
+my_string="abcdefghijklmnoptqrstuvwxyz"
+print(my_string.index('e')) #print number of location of 'e'
+print(my_string[4]) #print 4th letter in the list
