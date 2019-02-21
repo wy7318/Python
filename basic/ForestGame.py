1815b98fdb8e828279827f1b573fd431d2b1d76b
wy7318 <46336956+wy7318@users.noreply.github.com>
2019-02-21 12:58:14 -0700

    Update ForestGame.py

--- basic/ForestGame.py
+++ basic/ForestGame.py
@@ -5,12 +5,33 @@ locations = {0: "You are sitting in front of a computer learning Python",
              4: "You are in a valley beside a stream",
              5: "You are in the forest"}
 
-exits = [{"Q": 0},
-         {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
-         {"N": 5, "Q": 0},
-         {"W": 1, "Q": 0},
-         {"N": 1, "W": 2, "Q": 0},
-         {"W": 2, "S": 1, "Q": 0}]
+exits = {0: {"Q": 0},
+         1: {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
+         2: {"N": 5, "Q": 0},
+         3: {"W": 1, "Q": 0},
+         4: {"N": 1, "W": 2, "Q": 0},
+         5: {"W": 2, "S": 1, "Q": 0} }
+
+namedExits = {1: {"2": 2, "3": 3, "5": 5, "4": 4},
+              2: {"5": 5},
+              3: {"1": 1},
+              4: {"1": 1, "2": 2},
+              5: {"2": 2, "1": 1}}
+
+vocabulary = { "QUIT":  "Q",
+               "NORTH": "N",
+               "SOUTH": "S",
+               "EAST":  "E",
+               "WEST":  "W",
+               "ROAD": "1",
+               "HILL": "2",
+               "BUILDING": "3",
+               "VALLEY": "4",
+               "FOREST": "5" }
+
+# print(locations[0].split())
+# print(locations[3].split(","))
+# print(' '.join(locations[0].split()))
 
 loc = 1
 while True:
@@ -20,10 +41,21 @@ while True:
 
     if loc == 0:
         break
+    else:
+        allExits = exits[loc].copy()
+        allExits.update(namedExits[loc])
 
     direction = input("Available exits are " + availableExits + " ").upper()
     print()
-    if direction in exits[loc]:
-        loc = exits[loc][direction]
+    # Parse the user input, using our vocabulary dictionary if necessary
+    if len(direction) > 1:   # more than one letter, so check vocab
+        words = direction.split()
+        for word in words:
+            if word in vocabulary:
+                direction = vocabulary[word]
+                break
+
+    if direction in allExits:
+        loc = allExits[direction]
     else:
         print("You cannot go in that direction")
