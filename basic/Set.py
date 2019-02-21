29daf24f60761ebc11f2ed8e60e3ef9371d398ae
wy7318 <46336956+wy7318@users.noreply.github.com>
2019-02-20 22:43:21 -0700

    Update Set.py

--- basic/Set.py
+++ basic/Set.py
@@ -15,7 +15,7 @@ print(fruit)
 fruit.clear() #clearing whole set
 print(fruit)
 
-#Function of finding description of element of set
+#========Function of finding description of element of set================================
 while True:
         dict_key = input("please enter a fruit:")
         if dict_key == "quit":
@@ -24,3 +24,12 @@ while True:
                 print(fruit[dict_key])
         else:
                 print("Sorry, we don't have info of {}".format(dict_key))
+
+                
+#============================================================================================
+ordered_keys = list(fruit.keys()) #keys method returns values of object
+ordered_keys.sort() # this will make sure set will have same order(Alphabetical) each time
+for i in ordered_keys:
+        print(i + "-" + fruit[i])
+
+                
