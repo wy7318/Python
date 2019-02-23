1a8c25f580d7f02c70cc98e6d7edbb7a8afb6f0b
wy7318 <46336956+wy7318@users.noreply.github.com>
2019-02-22 19:01:29 -0700

    Update write.py

--- basic/write.py
+++ basic/write.py
@@ -5,3 +5,9 @@ with open("/Users/vazzo/Desktop/python/cities.txt", 'w') as city_file:
         print(city, file=city_file) #Create txt file
 
 print("Adelaide".strip('A')) #deleting 'A' and print leftover
+
+#=================================append into txt=============================================
+
+with open("/Users/vazzo/Desktop/python/sample.txt", 'a') as tables: #append times table
+    for i in range(1,13):
+        print("{} times {} is {}".format(i, 2, 1*2), file=tables)
