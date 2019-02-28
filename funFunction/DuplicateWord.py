28f2ea70aa58d1f6347e71332f8692940e30e128
wy7318 <46336956+wy7318@users.noreply.github.com>
2019-02-27 22:11:18 -0700

    Create DuplicateWord.py

---
+++ funFunction/DuplicateWord.py
@@ -0,0 +1,15 @@
+res="this Is what it is".lower().split()
+
+
+def isDuplicate(word):
+    hashTable ={}
+    dup=[]
+    for i in range(0,len(word)):
+        if word[i] not in hashTable:
+            hashTable[word[i]]=True #element insertion if not in hash table
+        else:
+            dup.append(word[i]) #if element in the hash table, we know it is duplicate
+
+    return dup
+
+print(isDuplicate(res))
