d9d6dde610024635f8da4104bea3f2c07b035b7a
wy7318 <46336956+wy7318@users.noreply.github.com>
2019-03-01 01:03:26 -0700

    Update FibnonacciDynamic.py

--- funFunction/FibnonacciDynamic.py
+++ funFunction/FibnonacciDynamic.py
@@ -1,9 +1,11 @@
 def fib(n):
+    if n == 0:
+        return 0
     if n == 1 or n == 2:
         return 1
     bottom = [None]*(n+1)
     bottom[1] = 1
-    bottom[2] = 2
+    bottom[2] = 1
     for i in range(3, n+1):
         bottom[i] = bottom[i-1]+bottom[i-2]
     return bottom[n]
