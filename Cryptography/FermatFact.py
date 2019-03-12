474d6985dc39c0bc41689b65c4b10726b64c6232
wy7318 <46336956+wy7318@users.noreply.github.com>
2019-03-12 13:59:20 -0700

    Update FermatFact.py

--- Cryptography/FermatFact.py
+++ Cryptography/FermatFact.py
@@ -1,10 +1,10 @@
 import math
 
 def fermat(n):
-    for p in range(1,101):
+    for p in range(1,10000):
         q = n + p**2
         if math.sqrt(q) % 1 == 0:
-            return p, int(math.sqrt(q))
+            return int(math.sqrt(q)) + p, int(math.sqrt(q)) - p
 
 
-print(fermat(91))
+print(fermat(8777))
