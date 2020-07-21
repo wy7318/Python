a0e1f6ebdb8b8a38f0037e1b4e62595b7bb0eef8
wy7318 <46336956+wy7318@users.noreply.github.com>
2020-07-21 16:50:39 -0700

    Update EnumerateDB.py
    Receive Data and enumerate. Count by Part Number

--- UsefulFunc/TableWidget/EnumerateDB.py
+++ UsefulFunc/TableWidget/EnumerateDB.py
@@ -28,3 +28,33 @@ def LoadData(self):
                 self.tableWidget_show.setItem(row_number, column_number, QtWidgets.QTableWidgetItem(str(data)))
 
         cur.close()
+
+        
+        #Count Stock from Database and display in another tablewidget
+def loadStock(self):
+        connection = mysql.connector.connect(host="22.55.66.777",
+                                             user="exampleUSER",
+                                             password="examplePW",
+                                             database="inventory"
+                                             )
+
+        query = "SELECT DISTINCT pn FROM inventory"
+        cur = connection.cursor()
+        cur.execute(query)
+        result = cur.fetchall()
+
+
+        self.tableWidget_stock.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.tableWidget_stock.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.tableWidget_stock.setItem(row_number, column_number, QtWidgets.QTableWidgetItem(str(data)))
+        for i in range(0,self.tableWidget_stock.rowCount()):
+            product = self.tableWidget_stock.item(i,0).text()
+            query = "SELECT * FROM inventory WHERE pn = %s"
+            values = (product,)
+            cur.execute(query, values)
+            countstock = str(len(cur.fetchall()))
+            self.tableWidget_stock.setItem(i,1, QTableWidgetItem(countstock))
+
+        cur.close()
