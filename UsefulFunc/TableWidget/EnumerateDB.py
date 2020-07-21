5ce86085369100899f1320e097d660a16e0128ca
wy7318 <46336956+wy7318@users.noreply.github.com>
2020-07-21 16:47:35 -0700

    Create EnumerateDB.py
    Receive data from MySQL server and enumerate into Tablewidget

---
+++ UsefulFunc/TableWidget/EnumerateDB.py
@@ -0,0 +1,30 @@
+#This example display database into tablewidget of PyQt
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QApplication, QWidget, QAction, QTableWidget,QTableWidgetItem,QVBoxLayout
+import InventoryGUI11
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+import mysql.connector
+
+def LoadData(self):
+        connection = mysql.connector.connect(host="45.79.70.200",
+                                             user="optilab",
+                                             password="Usopen97!",
+                                             database="inventory"
+                                             )
+
+        query = "SELECT * FROM inventory"
+
+        cur = connection.cursor()
+        cur.execute(query)
+        result = cur.fetchall()
+        self.tableWidget_show.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.tableWidget_show.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.tableWidget_show.setItem(row_number, column_number, QtWidgets.QTableWidgetItem(str(data)))
+
+        cur.close()
