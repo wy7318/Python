7e0519ee7d7a0b680f1dbe6cf08828b6b252ef0d
wy7318 <46336956+wy7318@users.noreply.github.com>
2020-07-21 16:40:42 -0700

    Update Barcode.py

--- UsefulFunc/Barcodes/Barcode.py
+++ UsefulFunc/Barcodes/Barcode.py
@@ -1,23 +1,19 @@
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.units import mm
-#I"ll be generating code39 barcodes, others are available
+
 from reportlab.graphics.barcode import code39
 import os
 
-# generate a canvas (A4 in this case, size doesn"t really matter)
-pagesize = (50*mm, 25*mm)
-c=canvas.Canvas("barcode_example1.pdf",pagesize=pagesize)
-# create a barcode object
-# (is not displayed yet)
-# The encode text is "123456789"
-# barHeight encodes how high the bars will be
-# barWidth encodes how wide the "narrowest" barcode unit is
+# generate a canvas 
+pagesize = (50*mm, 25*mm)     #Canvas Size
+c=canvas.Canvas("barcode_example1.pdf",pagesize=pagesize)     
+
 Manufacture = "OPTILAB"
 barcodetext = "B6P755464454"
 pn = "IML-1550-PM-V"
 barcode=code39.Extended39(barcodetext,barWidth=0.15*mm,barHeight=5*mm)
-# drawOn puts the barcode on the canvas at the specified coordinates
+
 
 text = c.beginText(1*mm, 20*mm)
 text.setFont("Courier", 9)
@@ -41,7 +37,7 @@ c.drawText(text1)
 
 
 
-# now create the actual PDF
+# create the actual PDF
 c.showPage()
 c.save()
 os.startfile("barcode_example1.pdf", "print")
