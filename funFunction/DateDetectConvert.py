9159fa330aaa2a416e73ca4724fdf90cefe012d9
wy7318 <46336956+wy7318@users.noreply.github.com>
2021-10-21 17:15:48 -0700

    Update DateDetectConvert.py
    Simple Data Conversion. 31st condition on each month detection is not implemented

--- funFunction/DateDetectConvert.py
+++ funFunction/DateDetectConvert.py
@@ -8,12 +8,23 @@ def Auto_todayOD(input):
             month = input.split("/")[0]     # Number before /
             date = input.split("/")[1]      # Number after /
             year = current_time.year
-            print(month + "-" + date + "-" + str(year))
+            if int(month) > 12 or int(date) > 31:
+                print("Incorrect Month")
+            else:
+                print(month + "-" + date + "-" + str(year))
         elif input.count("/") == 2:
             month = input.split("/")[0]     # Number before /
             date = input.split("/")[1]      # Number after /
             year = input.split("/")[2]      # Number after /
-            print(month + "-" + date + "-" + year)
+            if int(month) > 12 or int(date) > 31:       #Check if month and date is not out of format
+                print("Incorrect Month")
+            else:
+                if len(year) == 2:                      #Check if year format. 2 or 4 digit
+                    print(month + "-" + date + "-" + "20" + str(year))
+                elif len(year) == 4:
+                    print(month + "-" + date + "-" + year)
+                else:
+                    print("Incorrect Format")
         else:
             print("Incorrect Format")
     else:
