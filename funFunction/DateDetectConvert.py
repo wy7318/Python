611deffd27400a93fade305662c324e2f300d386
wy7318 <46336956+wy7318@users.noreply.github.com>
2021-10-21 17:27:23 -0700

    Update DateDetectConvert.py

--- funFunction/DateDetectConvert.py
+++ funFunction/DateDetectConvert.py
@@ -1,5 +1,3 @@
-import datetime
-
 def Auto_todayOD(input):
     current_time = datetime.datetime.now()
 
@@ -8,16 +6,21 @@ def Auto_todayOD(input):
             month = input.split("/")[0]     # Number before /
             date = input.split("/")[1]      # Number after /
             year = current_time.year
-            if int(month) > 12 or int(date) > 31:
-                print("Incorrect Month")
-            else:
-                print(month + "-" + date + "-" + str(year))
+            # Check and catch if date format is correct using datetime 
+            try:
+                t = month + "-" + date + "-" + str(year)
+                t2 = datetime.datetime.strptime(t,"%m-%d-%Y")
+                finalT = str(t2.strftime('%m-%d-%Y'))
+                print(finalT)
+            except:
+                print("Error")
+
         elif input.count("/") == 2:
             month = input.split("/")[0]     # Number before /
             date = input.split("/")[1]      # Number after /
             year = input.split("/")[2]      # Number after /
             if int(month) > 12 or int(date) > 31:       #Check if month and date is not out of format
-                print("Incorrect Month")
+                print("Incorrect Format")
             else:
                 if len(year) == 2:                      #Check if year format. 2 or 4 digit
                     print(month + "-" + date + "-" + "20" + str(year))
@@ -30,6 +33,6 @@ def Auto_todayOD(input):
     else:
         print("Incorrect Format")
 
-
+print(len("123"))
 date = input("Enter : ")
 Auto_todayOD(date)
